5d51d1c56883f50db22b44aa42072a86cf0b88af
juan morales <juanmorales0308200345@hotmail.com>
2021-09-23 19:43:14 -0600

    prueva de subida

--- MiNegosio/Win_MiNegosio/Form1.Designer.cs
+++ MiNegosio/Win_MiNegosio/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Win_MiNegosio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 232);
+            this.ClientSize = new System.Drawing.Size(482, 232);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
